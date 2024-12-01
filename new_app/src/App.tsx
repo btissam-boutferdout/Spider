93b417c79d2c735f8365889c0efdcc6c6892fab5
btissam-boutferdout <btissam.boutferdout@icloud.com>
2024-12-01 20:46:42 +0100

    first commit

---
+++ new_app/src/App.tsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Grid from "./components/Grid";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Grid />
+      </div>
+    </>
+  );
+}
+
+export default App;
